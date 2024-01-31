128797c124275cab3ab45d3ed7c073e312ad4674
msdpramod <kuppapramod@gmail.com>
2024-01-31 06:27:54 +0530

    hi

---
+++ src/main/java/org/commerceproject/euserservice/EUSerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.commerceproject.euserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EUSerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EUSerServiceApplication.class, args);
+    }
+
+}
